75f01a482e06e7def91e046d982b2f7e5d2c7ba9
ascaryaaa <allaamascarya@gmail.com>
2024-05-02 16:27:15 +0700

    first commit

---
+++ sb-tomcat/src/main/java/com/learn/sbtomcat/SbTomcatApplication.java
@@ -0,0 +1,13 @@
+package com.learn.sbtomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbTomcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbTomcatApplication.class, args);
+	}
+
+}
---
+++ sb-tomcat/src/test/java/com/learn/sbtomcat/SbTomcatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.sbtomcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbTomcatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
